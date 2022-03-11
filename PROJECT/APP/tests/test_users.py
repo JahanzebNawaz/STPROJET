5adca5adf970f37f099322ca1ec053d43b5f7779
jahanzebNawaz <jahanzaib.ch9996@gmail.com>
2022-03-11 16:18:23 -0500

    error test

--- PROJECT/APP/tests/test_users.py
+++ PROJECT/APP/tests/test_users.py
@@ -18,7 +18,7 @@ class UserTests(BaseTest):
 
     def test_update_user(self):
         user = User.objects.last()
-        self.assertEqual(user.id, 6)
+        self.assertEqual(user.id, 7)
         user.first_name = 'test'
         user.save()
         user = User.objects.last()
