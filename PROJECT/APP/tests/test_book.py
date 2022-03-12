b3b15edb77004ff625eebaefb9a7aa560dac697e
divyadommeti7 <dlsdivyaddsss@gmail.com>
2022-03-12 22:25:39 +0100

    added update and delete test

--- PROJECT/APP/tests/test_book.py
+++ PROJECT/APP/tests/test_book.py
@@ -18,6 +18,28 @@ class TestBook(BaseTest):
         self.assertEqual(book.count, 10)
         self.assertEqual(book.author.name, new_book.author.name)
 
+    def test_update_book(self):
+        self.assert_check_book(3)
+        self.add_new_book()
+        book = Book.objects.last()
+        self.assertEqual(book.count, 10)
+        self.assertEqual(book.subscription_cost, 6)
+        book.count = 20
+        book.subscription_cost = 8
+        book.save()
+        self.assertEqual(book.count, 20)
+        self.assertEqual(book.subscription_cost, 8)
+
+    def test_delete_book(self):
+        self.assert_check_book(3)
+        self.add_new_book()
+        book = Book.objects.last()
+        self.assertEqual(book.id, 4)
+        book.delete()
+        self.assert_check_book(3)
+        book = Book.objects.last()
+        self.assertEqual(book.id, 3)
+
     def add_new_book(self):
         title = 'Software testing: principles and practices'
         description = '"Software Testing: Principles and Practices is a comprehensive treatise on software testing.'
